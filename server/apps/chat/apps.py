fc711dd755d65d6df883df9a61f02e09628608f7
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-10-24 00:13:57 +0600

    ChatRoom and ChatMessage model created

--- server/apps/chat/apps.py
+++ server/apps/chat/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chat'
+    name = 'apps.chat'
--- server/apps/chat/models.py
+++ server/apps/chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from shortuuidfield import ShortUUIDField
+from apps.user.models import User
 
-# Create your models here.
+class ChatRoom(models.Model):
+	roomId = ShortUUIDField()
+	type = models.CharField(max_length=10, default='DM')
+	member = models.ManyToManyField(User)
+	name = models.CharField(max_length=20, null=True, blank=True)
+
+	def __str__(self):
+		return self.roomId + ' -> ' + str(self.name)
+
+class ChatMessage(models.Model):
+	chat = models.ForeignKey(ChatRoom, on_delete=models.SET_NULL, null=True)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	message = models.CharField(max_length=255)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.message
