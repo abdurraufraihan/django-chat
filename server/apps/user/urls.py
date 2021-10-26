408291e416697235a8879095c1de2ce99ddf9a9c
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-10-27 00:12:08 +0600

    user list api implemented

--- server/apps/user/urls.py
+++ server/apps/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.user.views import LoginApiView, SignupApiView
+from apps.user.views import UserView, LoginApiView, SignupApiView
 
 urlpatterns = [
+	path('users', UserView.as_view(), name='userList'),
 	path('login', LoginApiView.as_view(), name='login'),
 	path('signup', SignupApiView.as_view(), name='signup'),
 ]
--- server/apps/user/views.py
+++ server/apps/user/views.py
@@ -1,7 +1,27 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
 from apps.user.models import User
-from apps.user.serializers import LoginSerializer, SignupSerializer
+from apps.user.serializers import (
+	UserSerializer, LoginSerializer, SignupSerializer
+)
+
+class UserView(ListAPIView):
+	queryset = User.objects.all().order_by('first_name')
+	serializer_class = UserSerializer
+	pagination_class = LimitOffsetPagination
+
+	def get_queryset(self):
+		excludeUsersArr = []
+		try:
+			excludeUsers = self.request.query_params.get('exclude')
+			if excludeUsers:
+				userIds = excludeUsers.split(',')
+				for userId in userIds:
+					excludeUsersArr.append(int(userId))
+		except:
+			return []
+		return super().get_queryset().exclude(id__in=excludeUsersArr)
 
 class LoginApiView(TokenObtainPairView):
 	serializer_class = LoginSerializer
