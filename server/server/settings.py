972f9b5dfbac17277cea5cd4b54a8fb656c474cf
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-10-29 10:35:37 +0600

    added DEFAULT_AUTHENTICATION_CLASSES and DEFAULT_PERMISSION_CLASSES

--- server/server/settings.py
+++ server/server/settings.py
@@ -101,6 +101,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
