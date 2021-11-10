14248cb91df745e3e8b70b26b7c33f4b2c543e22
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-11 00:04:29 +0600

    added user chat to socket

--- server/apps/chat/consumers.py
+++ server/apps/chat/consumers.py
@@ -1,20 +1,28 @@
 import json
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from apps.chat.models import ChatRoom
 
 class ChatConsumer(AsyncWebsocketConsumer):
+	def getChatRoomsOfUser(self, userId):
+		return ChatRoom.objects.filter(member=userId)
+
 	async def connect(self):
 		self.userId = self.scope['url_route']['kwargs']['userId']
-		self.room_group_name = 'chat_%s' % self.userId
-		await self.channel_layer.group_add(
-			self.room_group_name,
-			self.userId
-		)
+		userRooms = await database_sync_to_async(
+			self.getChatRoomsOfUser
+		)(self.userId)
+		for room in userRooms:
+			await self.channel_layer.group_add(
+				room.roomId,
+				self.channel_name
+			)
 		await self.accept()
 
 	async def disconnect(self, close_code):
 		await self.channel_layer.group_discard(
 			self.room_group_name,
-			self.userId
+			self.channel_name
 		)
 
 	async def receive(self, text_data):
