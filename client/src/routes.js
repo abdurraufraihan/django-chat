679a8b2b1554577761d40da86df1724379f6878b
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-12 11:41:06 +0600

    moved login screen to login folder

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App";
-import LoginScreen from "./screens/LoginScreen";
+import LoginScreen from "./screens/login/LoginScreen";
 
 export default class Routes extends Component {
   render() {
