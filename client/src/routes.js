a5297ef38226def2b553a8952aed0b64c9cc3699
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-12 11:32:48 +0600

    resigned login screen and linked with routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App";
+import LoginScreen from "./screens/LoginScreen";
 
 export default class Routes extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class Routes extends Component {
       <Router>
         <Switch>
           <Route path="/" exact component={App} />
+          <Route path="/login" exact component={LoginScreen} />
         </Switch>
       </Router>
     );
---
+++ client/src/screens/LoginScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./loginScreen.css";
+
+const LoginScreen = () => {
+  return (
+    <div id="authFormContainer">
+      <div id="authForm">
+        <h2 id="authTitle">Login</h2>
+        <form>
+          <div className="authFieldContainer">
+            <input
+              className="authField"
+              type="email"
+              name="username"
+              placeholder="Email"
+            />
+          </div>
+          <div className="authFieldContainer">
+            <input
+              className="authField"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <br />
+          <button className="btn btn-outline-warning btn-block" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
