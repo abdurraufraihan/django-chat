a15d64874a21c4628c71874ce911c16a7348d575
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-17 01:37:09 +0600

    added validation to signup from with react-hook-form

--- client/src/screens/auth/signup/SignupScreen.js
+++ client/src/screens/auth/signup/SignupScreen.js
@@ -1,45 +1,87 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import "../authStyle.css";
 
 const SignupScreen = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+  const password = useRef({});
+  password.current = watch("password");
+  const image = watch("image");
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div id="authFormContainer">
       <div id="authForm">
         <h2 id="authTitle">Sign Up</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="authFieldContainer">
             <input
               className="authField"
               type="text"
-              name="first_name"
               placeholder="First Name"
+              {...register("first_name", { required: true })}
             />
+            {errors.first_name && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
           </div>
           <div className="authFieldContainer">
             <input
               className="authField"
               type="text"
-              name="last_name"
               placeholder="Last Name"
+              {...register("last_name", { required: true })}
             />
+            {errors.last_name && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
           </div>
           <div className="authFieldContainer">
             <input
               className="authField"
               type="email"
-              name="email"
               placeholder="Email"
+              {...register("email", { required: true })}
             />
+            {errors.email && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
           </div>
-          <input type="file" name="image" />
+          <div className="custom-file">
+            <input
+              type="file"
+              name="image"
+              id="validatedCustomFile"
+              {...register("image", {
+                required: "This field is required",
+              })}
+            />
+            <label className="custom-file-label" htmlFor="validatedCustomFile">
+              {image[0]?.name ? image[0]?.name : "Choose Image..."}
+            </label>
+            {errors.image && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
+          </div>
           <div className="authFieldContainer">
             <input
               className="authField"
               type="password"
-              name="password"
               placeholder="Password"
+              {...register("password", { required: true })}
             />
+            {errors.password && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
           </div>
           <div className="authFieldContainer">
             <input
@@ -47,7 +89,17 @@ const SignupScreen = () => {
               type="password"
               name="passwordTwo"
               placeholder="Confirm Password"
+              {...register("passwordTwo", {
+                required: "This field is required",
+                validate: (value) =>
+                  value === password.current || "The passwords doesn't match",
+              })}
             />
+            {errors.passwordTwo && (
+              <p className="requiredFieldError">
+                {errors.passwordTwo?.message}
+              </p>
+            )}
           </div>
           <br />
           <button className="btn btn-outline-warning btn-block" type="submit">
