2a4f747719e9576433b27f95a125dab7ca9a17dd
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-16 23:59:58 +0600

    validation added to login form

--- client/src/screens/auth/login/LoginScreen.js
+++ client/src/screens/auth/login/LoginScreen.js
@@ -1,28 +1,45 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import "../authStyle.css";
 
 const LoginScreen = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div id="authFormContainer">
       <div id="authForm">
         <h2 id="authTitle">Login</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="authFieldContainer">
             <input
               className="authField"
               type="email"
-              name="username"
               placeholder="Email"
+              {...register("username", { required: true })}
             />
+            {errors.username && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
           </div>
           <div className="authFieldContainer">
             <input
               className="authField"
               type="password"
-              name="password"
               placeholder="Password"
+              {...register("password", { required: true })}
             />
+            {errors.password && (
+              <p className="requiredFieldError">This field is required</p>
+            )}
           </div>
           <br />
           <button className="btn btn-outline-warning btn-block" type="submit">
