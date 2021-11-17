cc06dc6f1a0a94e741a0bdac95fa743615a93262
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-18 01:43:51 +0600

    cookie utility functions added

---
+++ client/src/util/cookieUtil.js
@@ -0,0 +1,29 @@
+const setCookie = (cookieName, cookieValue, expairydays = 30) => {
+  const today = new Date();
+  today.setTime(today.getTime() + expairydays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + today.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+};
+
+const getCookie = (cookieName) => {
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let cookieList = decodedCookie.split(";");
+  for (let i = 0; i < cookieList.length; i++) {
+    let cookie = cookieList[i];
+    while (cookie.charAt(0) == " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) == 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return "";
+};
+
+const CookieUtil = {
+  setCookie: setCookie,
+  getCookie: getCookie,
+};
+
+export default CookieUtil;
