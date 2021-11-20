9cc77997f7fa400a686b991cf4216a311aca4944
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-20 22:16:41 +0600

    saved message to database while passing through websocket

--- server/apps/chat/consumers.py
+++ server/apps/chat/consumers.py
@@ -1,9 +1,25 @@
 import json
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from apps.chat.models import ChatRoom
+from apps.chat.models import ChatRoom, ChatMessage
+from apps.user.models import User
 
 class ChatConsumer(AsyncWebsocketConsumer):
+	def saveMessage(self, message, userId, roomId):
+		userObj = User.objects.get(id=userId)
+		chatObj = ChatRoom.objects.get(roomId=roomId)
+		chatMessageObj = ChatMessage.objects.create(
+			chat=chatObj, user=userObj, message=message
+		)
+		return {
+			'user': userId,
+			'roomId': roomId,
+			'message': message,
+			'userImage': userObj.image.url,
+			'userName': userObj.first_name + " " + userObj.last_name,
+			'timestamp': str(chatMessageObj.timestamp)
+		}
+
 	async def connect(self):
 		self.userId = self.scope['url_route']['kwargs']['userId']
 		self.userRooms = await database_sync_to_async(
@@ -25,12 +41,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
 	async def receive(self, text_data):
 		text_data_json = json.loads(text_data)
 		message = text_data_json['message']
+		userId = text_data_json['user']
 		roomId = text_data_json['roomId']
+		chatMessage = await database_sync_to_async(
+			self.saveMessage
+		)(message, userId, roomId)
 		await self.channel_layer.group_send(
 			roomId,
 			{
 				'type': 'chat_message',
-				'message': message
+				'message': chatMessage
 			}
 		)
 
