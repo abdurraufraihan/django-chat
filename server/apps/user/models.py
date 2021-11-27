537e9f0af423e15ca17da8ac0f33142e2c13595a
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2021-11-27 13:26:19 +0600

    OnlineUser model initiated

--- server/apps/user/models.py
+++ server/apps/user/models.py
@@ -5,3 +5,9 @@ from shortuuidfield import ShortUUIDField
 class User(AbstractUser):
 	userId = ShortUUIDField()
 	image = models.ImageField(upload_to="user")
+
+class OnlineUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.user.username
